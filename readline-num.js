eace6a190a30e72da8d77cdf37925e96f81e0d5b
Tibor Krenacs <krenacs.tibor@gmail.com>
2019-06-21 11:18:19 +0200

    multiple

--- readline-num.js
+++ readline-num.js
@@ -1,4 +1,5 @@
 const ask = () => {
+  let readline = require('readLine-sync');
   let a = readline.question('Give me the first number!');
   let b = readline.question('Give me the second number!');
 };
